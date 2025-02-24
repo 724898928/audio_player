d8fcc7274eb971259b6a02f4ee832b9894aeefa1
care.lee <care.lee@provisionusa.com>
2025-02-24 21:32:36 +0800

    Change the context of the warninfo

--- lib/src/lee/view/PayView.dart
+++ lib/src/lee/view/PayView.dart
@@ -23,94 +23,101 @@ class _PayViewState extends State<PayView> {
       child: Card(
         child: Row(
           children: [
-            Container(
-              padding: EdgeInsets.only(left: 15, top: 50),
-              width: 50,
-              alignment: Alignment.topCenter,
-              child: VerticalText(text: "大哥哥大姐们啊, 你们都是有钱的人呐"),
-            ),
-            GestureDetector(
-              child: Container(
-                alignment: Alignment.center,
-                padding: EdgeInsets.all(0),
-                width: 300,
-                child: Column(
-                  children: [
-                    Container(
-                      color: Colors.red,
-                      //padding: EdgeInsets.only(top: 20, bottom: 20),
-                      child: Text(
-                        "打赏",
-                        style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            // color: Colors.red,
-                            backgroundColor: Colors.red),
-                      ),
-                      margin: EdgeInsets.all(10),
-                    ),
-                    isWeixin
-                        ? Image.memory(base64Decode(WeiXinImage))
-                        : Image.memory(base64Decode(ZhiFuBaoImage)),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+            Expanded(
+                flex: 1,
+                child: Container(
+                  width: 50,
+                  padding: EdgeInsets.only(left: 15, top: 50),
+                  alignment: Alignment.topCenter,
+                  child: VerticalText(text: "大哥哥大姐们啊, 你们都是有钱的人呐"),
+                )),
+            Expanded(
+                flex: 5,
+                child: GestureDetector(
+                  child: Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(0),
+                    width: 300,
+                    child: Column(
                       children: [
-                        Expanded(
-                          //flex: 1,
-                          // padding: EdgeInsets.all(0),
-                          // width: 120,
-                          // height: 50,
-                          child: ListTile(
-                            contentPadding: EdgeInsets.all(0),
-                            title: Text('微信扫码'),
-                            leading: Checkbox(
-                              value: isWeixin,
-                              onChanged: (v) {
-                                setState(() {
-                                  isWeixin = !isWeixin;
-                                });
-                              },
-                            ),
+                        Container(
+                          color: Colors.red,
+                          //padding: EdgeInsets.only(top: 20, bottom: 20),
+                          child: Text(
+                            "打赏",
+                            style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                // color: Colors.red,
+                                backgroundColor: Colors.red),
                           ),
+                          margin: EdgeInsets.all(10),
                         ),
-                        Expanded(
-                          //   flex: 1,
-                          // padding: EdgeInsets.all(0),
-                          // width: 250,
-                          // height: 50,
-                          child: ListTile(
-                            contentPadding: EdgeInsets.all(0),
-                            title: Text('支付宝扫码'),
-                            leading: Checkbox(
-                              value: !isWeixin,
-                              onChanged: (v) {
-                                setState(() {
-                                  isWeixin = !isWeixin;
-                                });
-                              },
+                        isWeixin
+                            ? Image.memory(base64Decode(WeiXinImage))
+                            : Image.memory(base64Decode(ZhiFuBaoImage)),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              //flex: 1,
+                              // padding: EdgeInsets.all(0),
+                              // width: 120,
+                              // height: 50,
+                              child: ListTile(
+                                contentPadding: EdgeInsets.all(0),
+                                title: Text('微信扫码'),
+                                leading: Checkbox(
+                                  value: isWeixin,
+                                  onChanged: (v) {
+                                    setState(() {
+                                      isWeixin = !isWeixin;
+                                    });
+                                  },
+                                ),
+                              ),
                             ),
-                          ),
-                        )
+                            Expanded(
+                              //   flex: 1,
+                              // padding: EdgeInsets.all(0),
+                              // width: 250,
+                              // height: 50,
+                              child: ListTile(
+                                contentPadding: EdgeInsets.all(0),
+                                title: Text('支付宝扫码'),
+                                leading: Checkbox(
+                                  value: !isWeixin,
+                                  onChanged: (v) {
+                                    setState(() {
+                                      isWeixin = !isWeixin;
+                                    });
+                                  },
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
                       ],
                     ),
-                  ],
-                ),
-              ),
-              onTap: () {
-                print("GestureDetector isWeixin:$isWeixin");
+                  ),
+                  onTap: () {
+                    print("GestureDetector isWeixin:$isWeixin");
 
-                callback?.call();
-                setState(() {
-                  isWeixin = !isWeixin;
-                });
-              },
-            ),
-            Container(
-              width: 50,
-              alignment: Alignment.topCenter,
-              padding: EdgeInsets.only(left: 15, top: 50),
-              child: VerticalText(text: "谁有那多余的零钱,给我这可怜的人啊"),
-            ),
+                    callback?.call();
+                    setState(() {
+                      isWeixin = !isWeixin;
+                    });
+                  },
+                )),
+            Expanded(
+              flex: 1,
+              child: Container(
+                width: 50,
+                alignment: Alignment.topCenter,
+                padding: EdgeInsets.only(left: 15, top: 50),
+                child: VerticalText(text: "谁有那多余的零钱,给我这可怜的人啊"),
+              ),
+            )
           ],
         ),
       ),
--- lib/src/lee/view/WarnInfo.dart
+++ lib/src/lee/view/WarnInfo.dart
@@ -10,13 +10,22 @@ class WarnInfo extends StatefulWidget {
 }
 
 class _WarnInfoState extends State<WarnInfo> {
+  final TextStyle style =
+      TextStyle(fontSize: 16, color: Colors.black, wordSpacing: 0);
   @override
   Widget build(BuildContext context) {
     return FeatureContext(
-      child: const Card(
-        child: Text("说明:\n"
-            "本播放器是本着学习和自用的目的开发的，请勿用于商业目的，若要用于商业目的请联系本人。\n"
-            "本播放器是使用Flutter开发的UI界面，使用Rust开发的播放逻辑服务。"),
+      child: Card(
+        child: Text(
+            "说明:\n"
+            "    本播放器是使用Flutter开发的UI界面,使用Rust开发的播放逻辑服务。\n"
+            "    本播放器是本着学习和自用的目的开发的，请勿用于商业目的，若要用于商业目的, 请联系本人。\n"
+            "    联系方式: 724898928@qq.com \n"
+            "    如果您有更好的建议或者意见，请将建议或意见发我邮箱或联系本人。\n"
+            "    如果您在使用中出现任何问题请将问题截图或文字方式发给我. \n"
+            "    如果您要更好的体验音乐，请购买正版音乐。\n"
+            "    本播放器的音乐来源于网络，如有侵权，请联系本人。\n",
+            style: style),
       ),
     );
   }
